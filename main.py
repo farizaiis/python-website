93d6215b6a5430d67af82561abac4403b43dae3a
farizaiis <farizaiis@hotmail.com>
2021-12-20 13:02:20 +0700

    test for set respon ui

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from website import create_app
 
 app = create_app()
 
+@app.route('/')
+def home():
+    return "Test"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    data = request.form
+    return render_template("login.html")
 
 @auth.route('logout')
 def logout():
-    return "<p>Logout</p?"
+    return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p?"
+    data = request.form
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = data.get('firstName')
+        password1 = data.get('password1')
+        password2 = data.get('password2')
+    return render_template("signup.html")
